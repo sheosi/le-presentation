3997ab170b26ba54888919677d4f99e59f550735
Sergio Tortosa Benedito <sertorbe@protonmail.com>
2026-03-03 16:27:02 +0100

    Add setting the limiter from the dashboard

--- src/dashboard.rs
+++ src/dashboard.rs
@@ -1,8 +1,13 @@
 pub fn main_dashboard() -> String {
     format!(
         r#"<html><head><meta charset=UTF-8></head><body>
-        <h1>Sistema de presentaciones</h1><form method=POST action=/dashboard>
+        <h1>Sistema de presentaciones</h1>
         <a href="/presentation">Ir a la presentación</a>
-        </form></body></html>"#
+
+        <form method="POST" action="/settings/limiter">
+            <input type="submit">Limiter</input>
+        </form>
+
+        </body></html>"#
     )
 }
